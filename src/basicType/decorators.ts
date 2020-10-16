249e701b2a96df4974764866bfd7f3e4b2f00905
SunRain <sunrain@SunRains-MacBook-Air.local>
2020-10-16 18:28:11 +0800

    update

--- src/basicType/decorators.ts
+++ src/basicType/decorators.ts
@@ -1,7 +1,7 @@
 // todo 装饰器
 //  ? 装饰器是一种特殊类型的声明，它能够被附加到类声明，方法，访问符，属性或参数上。
 // ? 装饰器使用@expression这种形式，expression求值后必须为一个函数，它会在运行时被调用，被装饰的声明信息作为参数传入
-function sealed(target) {
+function sealed1(target: any): void {
 
 }
 
@@ -18,7 +18,7 @@ function color(value: string) { // 这是一个装饰器工厂
  * ? 2. 求值的结果会被当做函数，由下至上依次调用
  */
 
-function f() {
+function f1() {
     console.info("f(): evaluated")
     return function (target, propertyKey: string, descriptor: PropertyDescriptor) {
         console.info("f(): called")
@@ -33,7 +33,7 @@ function g() {
 }
 
 class Co {
-    @f()
+    @f1()
     @g()
     method() {
 
@@ -53,3 +53,65 @@ class Co {
  * ?    3. 参数装饰器应用到构造函数
  * ?    4. 类装饰器应用到类
  */
+
+// todo 类装饰器
+// ? 类装饰器在类声明之前被声明
+// ? 类装饰器应用于类构造函数，可以用来监视，修改或替换类定义
+// ? 类装饰器不能用在声明文件中(.d.ts)，也不能用在任何外部上下文中(比如declare的类)
+
+@sealed
+class Greeter4 {
+    greeting: string
+    constructor(message: string) {
+        this.greeting = message
+    }
+    greet() {
+        return "Hello, " + this.greeting
+    }
+}
+
+function sealed(constructor: Function) {
+    // ? Object.seal密封一个对象，让其不能添加新属性，所有已有属性会变的不可配置。属性不可配置的效果就是属性变的不可删除，以及一个数据属性不能被重新定义成为访问器属性，或者反之。但属性的值仍然可以修改。
+    Object.seal(constructor)
+    Object.seal(constructor.prototype)
+}
+
+function classDecorator<T extends {new(...args: any[]):{}}>(constructor: T) {
+    return class extends constructor {
+        newProperty = "new property"
+        hello = "override"
+    }
+}
+@classDecorator
+class Greeter5 {
+    property = "property"
+    hello: string
+    constructor (m: string) {
+        this.hello = m
+    }
+}
+console.info(new Greeter("world"))
+
+// todo 方法装饰器
+// ? 方法装饰器声明在一个方法的声明之前（紧靠着方法声明）。它会被应用到方法的属性描述符上。可以用来监视，修改或者替换方法定义
+// ? 方法装饰器表达式会在运行时当作函数被调用，传入下列3个参数
+/**
+ * ? 1. 对于静态成员来说是类的构造函数，对于实例成员是类的原型对象
+ * ? 2. 成员的名字
+ * ? 3. 成员的属性描述符(如果输出目标版本小于ES5，属性描述符将会是undefined)
+ */
+
+class Greeter6 {
+    greeting: string
+    constructor(message: string) {
+        this.greeting = message
+    }
+    @enumerable(false)
+    greet() {
+        return "hello, " + this.greeting
+    }
+}
+
+function enumerable(value: boolean) {
+    return function ()
+}
